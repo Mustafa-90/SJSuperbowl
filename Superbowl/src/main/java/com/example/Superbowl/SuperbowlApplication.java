f24f04567278a337000618af045cd43c82642fcf
golfarn92 <mattias.hedin@gmail.com>
2019-08-06 16:33:46 +0200

    Min egna branch

--- Superbowl/src/main/java/com/example/Superbowl/SuperbowlApplication.java
+++ Superbowl/src/main/java/com/example/Superbowl/SuperbowlApplication.java
@@ -6,11 +6,11 @@ public class SuperbowlApplication {
 
     public static void main(String[] args) {
         int count = 0;
-        String connstr = "jdbc:sqlserver://localhost;databasename=Superbowl;user=admindb;password=123123";
+        String connstr = "jdbc:sqlserver://localhost;databasename=Superbowl;user=golfarn;password=123123";
 
         try (Connection conn = DriverManager.getConnection(connstr)) {
             Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("Select name FROM Video where id = 3");
+            ResultSet rs = st.executeQuery("Select name FROM Video");
 
             while (rs.next()) {
                 System.out.println(rs.getString("Name"));
