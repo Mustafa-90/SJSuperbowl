e7ee6e79403f9a396a56ea6d381846ba10996678
EriBas <erika.basso@hotmail.com>
2019-08-07 11:23:48 +0200

    test

--- Superbowl/src/main/java/com/example/Superbowl/SuperbowlApplication.java
+++ Superbowl/src/main/java/com/example/Superbowl/SuperbowlApplication.java
@@ -1,34 +1,10 @@
 
 package com.example.Superbowl;
-
-import java.sql.*;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
 public class SuperbowlApplication {
-
     public static void main(String[] args) {
-<<<<<<< .merge_file_a06408
         SpringApplication.run(SuperbowlApplication.class, args);
     }
-=======
-        int count = 0;
-        String connstr = "jdbc:sqlserver://localhost;databasename=Superbowl;user=dbadmin;password=123123";
->>>>>>> .merge_file_a06884
-
-        try (Connection conn = DriverManager.getConnection(connstr)) {
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("Select name FROM Video where id = 3");
-
-            while (rs.next()) {
-                System.out.println(rs.getString("Name"));
-            }
-
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 }
-
-
-
-
