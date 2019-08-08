fbf287294dbfee3e118bb46a6c2bbce11e50d1ba
Mustafa_H <mustafa@hamra.io>
2019-08-08 10:50:26 +0200

    Refactoring VideoController.java Removed Tag.java Updated Video constructor for search results.

--- Superbowl/src/main/java/com/example/Superbowl/Video.java
+++ Superbowl/src/main/java/com/example/Superbowl/Video.java
@@ -4,8 +4,19 @@ package com.example.Superbowl;
 public class Video {
 
     int id, companyId, genreId, categoryId, decade;
-    String embeddedUrl, videoName, Actor, Description;
+    String embeddedUrl, videoName, actor, description, companyName;
     boolean isPK;
+
+    public Video() {
+
+    }
+
+    public Video(String videoName, String description, String companyName, String embeddedUrl) {
+        this.embeddedUrl = embeddedUrl;
+        this.videoName = videoName;
+        this.description = description;
+        this.companyName = companyName;
+    }
 
     public Video(int id, int companyId, int genreId, int categoryId, String embeddedUrl, boolean isPK, int decade, String videoName, String actor, String description) {
         this.id = id;
@@ -15,8 +26,8 @@ public class Video {
         this.decade = decade;
         this.embeddedUrl = embeddedUrl;
         this.videoName = videoName;
-        Actor = actor;
-        Description = description;
+        this.actor = actor;
+        this.description = description;
         this.isPK = isPK;
     }
 
@@ -49,14 +60,18 @@ public class Video {
     }
 
     public String getActor() {
-        return Actor;
+        return actor;
     }
 
     public String getDescription() {
-        return Description;
+        return description;
     }
 
     public boolean isPK() {
         return isPK;
+    }
+
+    public String getCompanyName() {
+        return companyName;
     }
 }
--- Superbowl/src/main/java/com/example/Superbowl/VideoController.java
+++ Superbowl/src/main/java/com/example/Superbowl/VideoController.java
@@ -6,51 +6,107 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.awt.print.Book;
+import javax.servlet.http.HttpSession;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class VideoController {
 
-    Tag tagSearch = new Tag();
-    String connstr = "jdbc:sqlserver://localhost;databasename=Superbowl;user=dbadmin;password=123123";
+    //VideoModel vm = new VideoModel();
+    String connstr = "jdbc:sqlserver://localhost;databasename=Superbowl2;user=dbadmin;password=123123";
 
-    @GetMapping("/Cindy")
-    public String test(Model model) {
-        String result;
+
+    @GetMapping("/login")
+    public String getLogin(HttpSession session) {
+        String checkSession = (String) session.getAttribute("username");
+        if (checkSession != null) {
+            return "search";
+        } else {
+            return "login";
+        }
+    }
+
+    @PostMapping("/login")
+    public String postLogin(HttpSession session, @RequestParam String username, @RequestParam String password) {
+        String userLogin = "";
+        String userPassword = "";
+        String sql = "SELECT username, password\n" +
+                "FROM UserAccount\n" +
+                "WHERE username = ?\n" +
+                "AND password = ?";
+
         try (Connection conn = DriverManager.getConnection(connstr)) {
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("Select name FROM Video where id = 3");
-            while (rs.next()) {
-                result = rs.getString("Name");
-                model.addAttribute("result", result);
-                System.out.println(result);
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                userLogin = rs.getString(1);
+                userPassword = rs.getString(2);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return "test";
+        if (userLogin.equals(username.toLowerCase()) && userPassword.equals(password)) {
+            session.setAttribute("username", username);
+            return "SBindex";
+        } else {
+            return "login";
+        }
     }
 
     @GetMapping("/")
-    public String form(Model model) {
-        return "form";
+    public String getHomepage(HttpSession session) {
+        String checkSession = (String) session.getAttribute("username");
+        if (checkSession != null) {
+            return "search";
+        } else {
+            return "login";
+        }
+    }
+
+    @GetMapping("/search")
+    public String getSearch(HttpSession session) {
+        String checkSession = (String) session.getAttribute("username");
+        if (checkSession != null) {
+            return "search";
+        } else {
+            return "login";
+        }
     }
 
     @PostMapping("/search")
-    public String goSearch(Model model, @RequestParam String searchBar) {
+    public String postSearch(Model model, @RequestParam String searchBar) {
+
+        List<Video> listOfVideos = new ArrayList<>();
         String name = "", description = "", embeddedUrl = "", companyName = "";
+        String[] tags = new String[3];
+        String[] splitTags = searchBar.split(" ");
+
+        tags[0] = splitTags[0];
+        if (splitTags.length > 1)
+            tags[1] = splitTags[1];
+        if (splitTags.length > 2)
+            tags[2] = splitTags[2];
+
 
         String sql = "SELECT name, Description, embedded_URL, companyName\n" +
                 "FROM Video\n" +
                 "JOIN Company ON company_Id = Company.id\n" +
                 "JOIN Genre on genre_Id = Genre.id\n" +
                 "JOIN category on category_id = category.id\n" +
-                "WHERE Description LIKE ?\n";
+                "WHERE searchtag like ?\n" +
+                "OR searchtag like ?\n" +
+                "OR searchtag like ?";
 
         try (Connection conn = DriverManager.getConnection(connstr)) {
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, "%" + searchBar + "%");
+            ps.setString(1, "%" + tags[0] + "%");
+            ps.setString(2, "%" + tags[1] + "%");
+            ps.setString(3, "%" + tags[2] + "%");
             ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
@@ -58,17 +114,19 @@ public class VideoController {
                 description = rs.getString(2);
                 embeddedUrl = rs.getString(3);
                 companyName = rs.getString(4);
+                listOfVideos.add(new Video(name, description, companyName, embeddedUrl));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        model.addAttribute("name", name);
-        model.addAttribute("description", description);
-        model.addAttribute("embedded", embeddedUrl);
-        model.addAttribute("companyname", companyName);
-
+        model.addAttribute("listOfVideos", listOfVideos);
         return "search";
     }
 
+    @PostMapping("/logout")
+    public String getLogout(HttpSession session) {
+        session.invalidate();
+        return "login";
+    }
 }
